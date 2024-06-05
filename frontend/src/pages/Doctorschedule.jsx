f428a8b7a2fe5b7bc993c634716996b1e170cdfc
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-05 23:08:14 +0800

    Menambahkan Doctorschedule

---
+++ frontend/src/pages/Doctorschedule.jsx
@@ -0,0 +1,16 @@
+import NavbarComponent from "../components/NavbarComponent";
+import FooterComponent from "../components/FooterComponent";
+
+import DoctorscheduleComponent from "../components/DoctorscheduleComponent";
+
+function Doctorschedule() {
+  return (
+    <div>
+      <NavbarComponent />
+      <DoctorscheduleComponent />
+      <FooterComponent />
+    </div>
+  );
+}
+
+export default Doctorschedule;
