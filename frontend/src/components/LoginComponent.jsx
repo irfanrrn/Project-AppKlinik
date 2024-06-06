1708113cc0762f9b1607213a2940e4d196680ea3
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-06 23:58:30 +0800

    Menambahkan Form Login

---
+++ frontend/src/components/LoginComponent.jsx
@@ -0,0 +1,51 @@
+import dokterImg from "../assets/img/doctor-masuk.jpg";
+
+const LoginComponent = () => {
+  return (
+    <div>
+      <div className="container1">
+        <div className="login-form">
+          <h1>Login to Account</h1>
+          <p>Welcome back, Please log in to your Account.</p>
+          <form>
+            <label for="email">EMAIL</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              placeholder="full name"
+              required
+            />
+
+            <label for="password">PASSWORD</label>
+            <input
+              type="password"
+              id="password"
+              name="password"
+              placeholder="password"
+              required
+            />
+
+            <a href="#" className="forgot-password">
+              Forgot password?
+            </a>
+
+            <div className="buttons">
+              <button type="submit" className="login-btn">
+                LOGIN ACCOUNT
+              </button>
+              <button type="button" className="register-btn">
+                REGISTER ACCOUNT
+              </button>
+            </div>
+          </form>
+        </div>
+        <div className="login-image">
+          <img src={dokterImg} alt="Doctor" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginComponent;
