23268eeac1658bb39e528b740331b116b03050c5
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-07 11:42:17 +0800

    Manambahkan Form Register

---
+++ frontend/src/pages/Register.jsx
@@ -0,0 +1,11 @@
+import RegisterComponent from "../components/RegisterComponent";
+
+function Register() {
+  return (
+    <div>
+      <RegisterComponent />
+    </div>
+  );
+}
+
+export default Register;
