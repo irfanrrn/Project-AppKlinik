b6035c879bdcab455f13462bbb4676de4dd70f58
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-10 16:29:45 +0800

    Menambahkan Page About Us

---
+++ frontend/src/pages/AboutUs.jsx
@@ -0,0 +1,16 @@
+import NavbarComponent from "../components/NavbarComponent";
+import FooterComponent from "../components/FooterComponent";
+
+import AboutUsComponent from "../components/AboutUsComponent";
+
+function AboutUs() {
+  return (
+    <div>
+      <NavbarComponent />
+      <AboutUsComponent />
+      <FooterComponent />
+    </div>
+  );
+}
+
+export default AboutUs;
