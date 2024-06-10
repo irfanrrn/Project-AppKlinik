61df207b19f9fea334bb3380b7be73b0d91f0ee6
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-10 16:34:36 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,14 @@ import Home from "./pages/Home";
 import Doctorschedule from "./pages/Doctorschedule";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import AboutUs from "./pages/AboutUs";
+
+import "@fortawesome/fontawesome-free/css/all.min.css";
+
+import "animate.css/animate.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "owl.carousel/dist/assets/owl.carousel.min.css";
+import "tempusdominus-bootstrap-4/build/css/tempusdominus-bootstrap-4.min.css";
 
 import "./scss/styles.scss";
 import * as bootstrap from "bootstrap";
@@ -15,6 +23,7 @@ function App() {
         <Route path="/Doctorschedule" element={<Doctorschedule />} />
         <Route path="/Login" element={<Login />} />
         <Route path="/Register" element={<Register />} />
+        <Route path="/AboutUs" element={<AboutUs />} />
       </Routes>
     </BrowserRouter>
   );
