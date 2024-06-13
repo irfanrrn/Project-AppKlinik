49ba67063384a89626dae92ac909fb412e57ad7d
RizkiRaihan13 <raihanrizki83@gmail.com>
2024-06-13 19:41:01 +0700

    update login

--- backend/controller/loginController.js
+++ backend/controller/loginController.js
@@ -1,40 +1,33 @@
 var connection = require('../library/database');
-var crypto = require('crypto');
 
-const createLogin = function(req, res){
+const createLogin = function (req, res) {
     const {email, password} = req.body;
 
-    connection.query('SELECT * FROM tbl_users WHERE email = ?', [email], async (err, userResults) => {
+    connection.query('SELECT * FROM tbl_users WHERE email = ? AND password = SHA2(?, 512)', [email, password], async (err, userResults) => {
         if (err) {
-          res.status(500).json({ message: 'Error logging in' });
+            res.status(500).json({message: 'Error logging in'});
         } else if (userResults.length > 0) {
-          const user = userResults[0];
-          const match = await crypto.createHash('sha256').update(password).digest('hex');
-    
-          if (match) {
+            const user = userResults[0];
             req.session.user = user;
-            res.status(200).json({ message: 'Login successful', user: { id: user.id, username: user.username, email: user.email, role: 'user' } });
-          } else {
-            res.status(401).json({ message: 'Invalid email or password' });
-          }
+            res.status(200).json({
+                message: 'Login successful',
+                user: {id: user.id, username: user.username, email: user.email, role: 'user'}
+            });
         } else {
-          connection.query('SELECT * FROM tbl_administrators WHERE email = ?', [email], async (err, adminResults) => {
-            if (err) {
-              res.status(500).json({ message: 'Error logging in' });
-            } else if (adminResults.length > 0) {
-              const admin = adminResults[0];
-              const match = await crypto.createHash('sha256').update(password).digest('hex');
-    
-              if (match) {
-                req.session.admin = admin;
-                res.status(200).json({ message: 'Login successful', user: { id: admin.id, username: admin.username, email: admin.email, role: 'admin' } });
-              } else {
-                res.status(401).json({ message: 'Invalid email or password' });
-              }
-            } else {
-              res.status(401).json({ message: 'user not found' });
-            }
-          });
+            connection.query('SELECT * FROM tbl_administrators WHERE email = ?  AND password = SHA2(?, 512)', [email, password], async (err, adminResults) => {
+                if (err) {
+                    res.status(500).json({message: 'Error logging in'});
+                } else if (adminResults.length > 0) {
+                    const admin = adminResults[0];
+                    req.session.admin = admin;
+                    res.status(200).json({
+                        message: 'Login successful',
+                        user: {id: admin.id, username: admin.username, email: admin.email, role: 'admin'}
+                    });
+                } else {
+                    res.status(400).json({message: 'Invalid email or password'});
+                }
+            });
         }
     });
 }
