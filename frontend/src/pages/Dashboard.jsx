0c79251ff97aee84d9701547c2483e95a804a6ed
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-13 16:56:09 +0800

    dashboard

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import DashboardComponent from "../components/DashboardComponent";
+
+function Dashboard() {
+  return (
+    <div>
+      <Dashboard />
+    </div>
+  );
+}
+
+export default Dashboard;
