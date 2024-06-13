5957bfd3781c5f71a5fd5e5d2a43785033bc3628
Irfan_rrn <irfanrrn@upi.edu>
2024-06-13 13:28:19 +0700

    update app

---
+++ frontend/src/pages/AppointmentAll.jsx
@@ -0,0 +1,16 @@
+import NavbarComponent from "../components/NavbarComponent";
+import FooterComponent from "../components/FooterComponent";
+
+import AppointmentAllComponent from "../components/AppointmentAllComponent";
+
+function AppointmentAll() {
+  return (
+    <div>
+      <NavbarComponent />
+      <AppointmentAllComponent />
+      <FooterComponent />
+    </div>
+  )
+}
+
+export default AppointmentAll
---
+++ frontend/src/pages/FormAppointment.jsx
@@ -0,0 +1,16 @@
+import NavbarComponent from "../components/NavbarComponent";
+import FooterComponent from "../components/FooterComponent";
+
+import AppointmentForm from "../components/AppointmentForm";
+
+function FormAppointment() {
+  return (
+    <div>
+      <NavbarComponent />
+      <AppointmentForm />
+      <FooterComponent />
+    </div>
+  )
+}
+
+export default FormAppointment
