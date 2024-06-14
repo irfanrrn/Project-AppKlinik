80d0ad4e4e8f130ddcdb7ebc46b943446dcdd89b
Irfan_rrn <irfanrrn@upi.edu>
2024-06-14 19:14:56 +0700

    update appoint jsx

---
+++ frontend/src/pages/AppointmentCan.jsx
@@ -0,0 +1,16 @@
+import NavbarComponent from "../components/NavbarComponent";
+import FooterComponent from "../components/FooterComponent";
+
+import AppointmentCanComponent from "../components/AppointmentCanComponent";
+
+function AppointmentCan() {
+  return (
+    <div>
+      <NavbarComponent />
+      <AppointmentCanComponent />
+      <FooterComponent />
+    </div>
+  )
+}
+
+export default AppointmentCan
---
+++ frontend/src/pages/AppointmentCom.jsx
@@ -0,0 +1,17 @@
+import NavbarComponent from "../components/NavbarComponent";
+import FooterComponent from "../components/FooterComponent";
+import AppointmentComComponent from "../components/AppointmentComComponent";
+
+
+
+function AppointmentCom() {
+  return (
+    <div>
+      <NavbarComponent />
+      <AppointmentComComponent />
+      <FooterComponent />
+    </div>
+  )
+}
+
+export default AppointmentCom
