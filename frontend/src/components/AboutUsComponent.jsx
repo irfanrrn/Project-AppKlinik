d5430aff2616d2ee25c584ce091e862c200eb08f
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-15 22:05:23 +0800

    Update AboutUsComponent.jsx

--- frontend/src/components/AboutUsComponent.jsx
+++ frontend/src/components/AboutUsComponent.jsx
@@ -35,7 +35,7 @@ const AboutUsComponent = () => {
             <div className="col-lg-6 wow fadeIn" data-wow-delay="0.1s">
               <div className="d-flex flex-column">
                 <img
-                  className="img-fluid rounded w-75 align-self-end"
+                  className="img-fluid rounded w-100 align-self-end"
                   src={aboutImg}
                   alt=""
                 />
