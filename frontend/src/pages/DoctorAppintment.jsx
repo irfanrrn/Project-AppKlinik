27cd5fe13c5bffaccd17538e6d1f195b11e68cc6
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-15 14:42:48 +0800

    update dashboard

---
+++ frontend/src/pages/DoctorAppintment.jsx
@@ -0,0 +1,13 @@
+import DashboardComponent from "../components/DashboardComponent";
+import DoctorAppointmentComponent from "../components/DoctorAppointmentComponent";
+
+function DoctorAppintment() {
+  return (
+    <div>
+      <DashboardComponent />
+      <DoctorAppointmentComponent />
+    </div>
+  );
+}
+
+export default DoctorAppintment;
