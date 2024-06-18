e196f755ff3b46443d4bc7144cf984497e9ca5f8
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-18 17:39:47 +0800

    menambahkan patient edit

---
+++ frontend/src/pages/PatientEdit1.jsx
@@ -0,0 +1,11 @@
+import PatientEdit1Component from "../components/PatientEdit1Component";
+
+function PatientEdit1() {
+  return (
+    <div>
+      <PatientEdit1Component />
+    </div>
+  );
+}
+
+export default PatientEdit1;
---
+++ frontend/src/pages/PatientEdit2.jsx
@@ -0,0 +1,11 @@
+import PatientEdit2Component from "../components/PatientEdit2Component";
+
+function PatientEdit2() {
+  return (
+    <div>
+      <PatientEdit2Component />
+    </div>
+  );
+}
+
+export default PatientEdit2;
