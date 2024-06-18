7d0916bfa784d353a096437ce88ccf7493bbf87f
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-18 17:37:33 +0800

    menambahkan doctor edit

---
+++ frontend/src/pages/DoctorEdit2.jsx
@@ -0,0 +1,11 @@
+import DoctorEdit2Component from "../components/DoctorEdit2Component";
+
+function DoctorEdit2() {
+  return (
+    <div>
+      <DoctorEdit2Component />
+    </div>
+  );
+}
+
+export default DoctorEdit2;
---
+++ frontend/src/pages/DoctorEdit5.jsx
@@ -0,0 +1,11 @@
+import DoctorEdit5Component from "../components/DoctorEdit5Component";
+
+function DoctorEdit5() {
+  return (
+    <div>
+      <DoctorEdit5Component />
+    </div>
+  );
+}
+
+export default DoctorEdit5;
