4d71d1277ff9fec93eaa60d3d957ec2734fa0187
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-18 19:25:44 +0800

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -51,46 +51,6 @@ import MdViewApp4 from "./pages/MdViewApp4";
 import MdViewApp5 from "./pages/MdViewApp5";
 import MdViewApp6 from "./pages/MdViewApp6";
 
-import DoctorEdit1 from "./pages/DoctorEdit1";
-import DoctorEdit2 from "./pages/DoctorEdit2";
-import DoctorEdit3 from "./pages/DoctorEdit3";
-import DoctorEdit4 from "./pages/DoctorEdit4";
-import DoctorEdit5 from "./pages/DoctorEdit5";
-import DoctorEdit6 from "./pages/DoctorEdit6";
-
-import DoctorScheduleEdit1 from "./pages/DoctorScheduleEdit1";
-import DoctorScheduleEdit2 from "./pages/DoctorScheduleEdit2";
-import DoctorScheduleEdit3 from "./pages/DoctorScheduleEdit3";
-import DoctorScheduleEdit4 from "./pages/DoctorScheduleEdit4";
-import DoctorScheduleEdit5 from "./pages/DoctorScheduleEdit5";
-
-import PatientEdit1 from "./pages/PatientEdit1";
-import PatientEdit2 from "./pages/PatientEdit2";
-import PatientEdit3 from "./pages/PatientEdit3";
-import PatientEdit4 from "./pages/PatientEdit4";
-import PatientEdit5 from "./pages/PatientEdit5";
-
-import AppointmentEdit1 from "./pages/AppointmentEdit1";
-import AppointmentEdit2 from "./pages/AppointmentEdit2";
-import AppointmentEdit3 from "./pages/AppointmentEdit3";
-import AppointmentEdit4 from "./pages/AppointmentEdit4";
-import AppointmentEdit5 from "./pages/AppointmentEdit5";
-import AppointmentEdit6 from "./pages/AppointmentEdit6";
-
-import UserEdit1 from "./pages/UserEdit1";
-import UserEdit2 from "./pages/UserEdit2";
-import UserEdit3 from "./pages/UserEdit3";
-import UserEdit4 from "./pages/UserEdit4";
-import UserEdit5 from "./pages/UserEdit5";
-import UserEdit6 from "./pages/UserEdit6";
-
-import AdminEdit1 from "./pages/AdminEdit1";
-import AdminEdit2 from "./pages/AdminEdit2";
-import AdminEdit3 from "./pages/AdminEdit3";
-import AdminEdit4 from "./pages/AdminEdit4";
-import AdminEdit5 from "./pages/AdminEdit5";
-import AdminEdit6 from "./pages/AdminEdit6";
-
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
 import "animate.css/animate.min.css";
@@ -152,46 +112,7 @@ function App() {
         <Route path="/view-appointmnet5" element={<MdViewApp5 />} />
         <Route path="/view-appointmnet6" element={<MdViewApp6 />} />
 
-       <Route path="/doctorEdit1" element={<DoctorEdit1 />} />
-        <Route path="/doctorEdit2" element={<DoctorEdit2 />} />
-        <Route path="/doctorEdit3" element={<DoctorEdit3 />} />
-        <Route path="/doctorEdit4" element={<DoctorEdit4 />} />
-        <Route path="/doctorEdit5" element={<DoctorEdit5 />} />
-        <Route path="/doctorEdit6" element={<DoctorEdit6 />} />
-
-        <Route path="/doctorScheduleEdit1" element={<DoctorScheduleEdit1 />} />
-        <Route path="/doctorScheduleEdit2" element={<DoctorScheduleEdit2 />} />
-        <Route path="/doctorScheduleEdit3" element={<DoctorScheduleEdit3 />} />
-        <Route path="/doctorScheduleEdit4" element={<DoctorScheduleEdit4 />} />
-        <Route path="/doctorScheduleEdit5" element={<DoctorScheduleEdit5 />} />
-
-        <Route path="/patientEdit1" element={<PatientEdit1 />} />
-        <Route path="/patientEdit2" element={<PatientEdit2 />} />
-        <Route path="/patientEdit3" element={<PatientEdit3 />} />
-        <Route path="/patientEdit4" element={<PatientEdit4 />} />
-        <Route path="/patientEdit5" element={<PatientEdit5 />} />
-
-        <Route path="/appointmentedit1" element={<AppointmentEdit1 />} />
-        <Route path="/appointmentedit2" element={<AppointmentEdit2 />} />
-        <Route path="/appointmentedit3" element={<AppointmentEdit3 />} />
-        <Route path="/appointmentedit4" element={<AppointmentEdit4 />} />
-        <Route path="/appointmentedit5" element={<AppointmentEdit5 />} />
-        <Route path="/appointmentedit6" element={<AppointmentEdit6 />} />
-
-        <Route path="/useredit1" element={<UserEdit1 />} />
-        <Route path="/useredit2" element={<UserEdit2 />} />
-        <Route path="/useredit3" element={<UserEdit3 />} />
-        <Route path="/useredit4" element={<UserEdit4 />} />
-        <Route path="/useredit5" element={<UserEdit5 />} />
-        <Route path="/useredit6" element={<UserEdit6 />} />
-
-        <Route path="/adminEdit1" element={<AdminEdit1 />} />
-        <Route path="/adminEdit2" element={<AdminEdit2 />} />
-        <Route path="/adminEdit3" element={<AdminEdit3 />} />
-        <Route path="/adminEdit4" element={<AdminEdit4 />} />
-        <Route path="/adminEdit5" element={<AdminEdit5 />} />
-        <Route path="/adminEdit6" element={<AdminEdit6 />} />
-
+       
       </Routes>
     </BrowserRouter>
   );
