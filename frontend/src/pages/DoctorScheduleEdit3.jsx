6a9a47ad6e52b580a32d34710b7b6d552d3bb76a
Khotib12 <164130759+Khotib12@users.noreply.github.com>
2024-06-18 17:38:43 +0800

    menambahkan doctor schedule edit

---
+++ frontend/src/pages/DoctorScheduleEdit3.jsx
@@ -0,0 +1,11 @@
+import DoctorScheduleEdit3Component from "../components/DoctorScheduleEdit3Component";
+
+function DoctorScheduleEdit3() {
+  return (
+    <div>
+      <DoctorScheduleEdit3Component />
+    </div>
+  );
+}
+
+export default DoctorScheduleEdit3;
---
+++ frontend/src/pages/DoctorScheduleEdit4.jsx
@@ -0,0 +1,11 @@
+import DoctorScheduleEdit4Component from "../components/DoctorScheduleEdit4Component";
+
+function DoctorScheduleEdit4() {
+  return (
+    <div>
+      <DoctorScheduleEdit4Component />
+    </div>
+  );
+}
+
+export default DoctorScheduleEdit4;
