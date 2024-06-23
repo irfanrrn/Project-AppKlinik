740e131aa18aeda3b55ff969937bff214833dcbe
RizkiRaihan13 <raihanrizki83@gmail.com>
2024-06-23 21:42:44 +0700

    update koneksi database

--- backend/library/database.js
+++ backend/library/database.js
@@ -6,7 +6,7 @@
 //     database            : 'klinik-app'
 // };
 
-let mysql = require('mysql');
+let mysql = require('mysql2');
  
 let connection = mysql.createConnection({
    host:        'educalab.id',
@@ -14,7 +14,7 @@ let connection = mysql.createConnection({
    password:    'Wm0T0gxgDTtAyODS',
    database:    '1LIUp1PCfwXo6Of6',
    port: 3307
- });
+});
 
 // let connection = mysql.createConnection({
 //   host:        'localhost',
